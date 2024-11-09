e2243f990aeae9fb41df3eaca6f80c038c85a1b4
Mohammed Shamil <mohammedshamil0300@gmail.com>
2024-11-09 20:23:16 +0530

    feat(ticketRoute): add route for ticket complete

--- src/routes/ticketRoute.ts
+++ src/routes/ticketRoute.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { handleInitiateTicket } from "../controllers/ticketController";
+import {
+  handleCompleteTicket,
+  handleInitiateTicket,
+} from "../controllers/ticketController";
 import { authorize } from "../helpers/authorise";
 
 const router = express.Router();
 
 router.post("/initiate", authorize(["TICKETER"]), handleInitiateTicket);
+router.post("/complete", authorize(["TICKETER"]), handleCompleteTicket);
 
 export default router;
